c064c40ac505dc9d081361543db37764fa88c9c6
Biplob hasan <fbiplobhasan2024@gmail.com>
2025-07-11 20:20:21 +0600

    add pets website functionality like: nav,banner,spinner,card,sort,add active and dactive funtionaliry

---
+++ script.js
@@ -0,0 +1,89 @@
+const loadAllCategoryBtn = async () => {
+    const res = await fetch('https://openapi.programming-hero.com/api/peddy/categories')
+    const data = await res.json()
+    displayCategoryBtn(data.categories);
+}
+
+const loadPetsByCategory = async (category) => {
+    removeActiveClass()
+    addActiveClasses(category)
+    loadingSpinner(true)
+    const res = await fetch(`https://openapi.programming-hero.com/api/peddy/category/${category}`)
+    const data = await res.json()
+    setTimeout(() => {
+        displayAllPets(data.data);
+        storedPetsData = data.data
+        loadingSpinner(false)
+    }, 200)
+}
+
+const loadAllPets = async () => {
+
+    
+
+    loadingSpinner(true)
+    const res = await fetch('https://openapi.programming-hero.com/api/peddy/pets')
+    const data = await res.json()
+    setTimeout(() => {
+        displayAllPets(data.pets);
+        storedPetsData = data.pets
+        loadingSpinner(false)
+    }, 200)
+}
+
+
+const displayAllPets = (data) => {
+
+    const cardContainer = document.getElementById('card-container')
+
+    if (data.length == 0) {
+        cardContainer.classList.remove('grid')
+        cardContainer.innerHTML = `
+        <div class="flex flex-col justify-center items-center mx-auto">
+        <img src="./images/error.webp" alt="">
+        <h1>Not Available This Category Content.</h1>
+    </div>
+        `;
+    } else {
+        cardContainer.classList.add('grid')
+    }
+
+    data.forEach(card => {
+        const div = document.createElement('div')
+        div.classList.add('p-5', 'rounded-lg', 'border')
+        div.innerHTML = `
+          <img class="h-36 w-full rounded-xl" src=${card.image} alt="">
+           <p class="font-bold">Name:${card.pet_name}</p>
+           <p class="font-bold">Breed:${card.breed ? card.breed : 'Not Available'}</p>
+           <p class="font-bold">Vaccinated:${card.vaccinated_status}</p>
+           <p class="font-bold">Gender:${card.gender}</p>
+           <p class="font-bold">Price:${card.price}</p>
+           <p class="font-bold">Date of birth:${card.date_of_birth ? card.date_of_birth : 'Not Available'}</p>
+           <p class="font-bold">Category:${card.category}</p>
+           <div class="flex justify-between overflow-hidden mt-5">
+           <button class="border">Adopt</button>
+           <button class="border">Like</button>
+           <button class="border">Details</button>
+            </div>
+        `
+        cardContainer.appendChild(div)
+    })
+}
+
+const displayCategoryBtn = (data) => {
+    const btnContainer = document.getElementById('btn-container')
+
+    data.forEach(btn => {
+        const div = document.createElement('div')
+        div.innerHTML = `
+        <button id="${btn.category}" onClick="loadPetsByCategory('${btn.category}')" class="category-btn flex btn gap-6 font-bold lg:gap-10">
+            <img class="w-100 h-full" src=${btn.category_icon} alt="">
+            <p>${btn.category}</p>
+        </button>
+        `
+        btnContainer.appendChild(div)
+    });
+}
+
+loadAllCategoryBtn()
+loadAllPets()
---
+++ utilities.js
@@ -0,0 +1,43 @@
+let storedPetsData = [];
+
+const loadingSpinner = (show) => {
+    const spinner = document.getElementById('loader')
+    if (show) {
+        spinner.classList.remove('hidden')
+        const cardContainer = document.getElementById('card-container').innerHTML = '';
+    } else {
+        spinner.classList.add('hidden')
+    }
+}
+
+const removeActiveClass = () => {
+    const allButtons = document.querySelectorAll('.category-btn')
+
+    for (btn of allButtons) {
+        btn.classList.remove(
+            'bg-emerald-100',
+            'rounded-full',
+            'border-teal-800',
+            'border-2',
+        )
+    }
+}
+
+const addActiveClasses = (category) => {
+    const activeButtons = document.getElementById(`${category}`)
+    activeButtons.classList.add(
+        'bg-emerald-100',
+        'rounded-full',
+        'border-teal-800',
+        'border-2',
+    )
+}
+
+const sort = () => {
+    loadingSpinner(true)
+    const sortedData = storedPetsData.sort((a, b) => b.price - a.price)
+    setTimeout(() => {
+        displayAllPets(sortedData);
+        loadingSpinner(false)
+    }, 200)
+}
